7fac586da840a3556e073bac3c96c6f66eac2d82
c-vallejo <charlytandil@gmail.com>
2019-11-28 11:01:14 -0300

    fix en el addComentario

--- js/film.js
+++ js/film.js
@@ -25,11 +25,6 @@ let app = new Vue({
 
     function addComentario(e){
         e.preventDefault();
-        let id_film = document.querySelector("#id_film").getAttribute('data');
-        let nombre_usuario = document.querySelector("#nombre_usuario").getAttribute('data');
-        let comentario = document.querySelector("#comentario").value;
-        let puntuacion = document.querySelector("#puntuacion").value;
-
 
         let id_film = document.querySelector("#id_film").getAttribute('data');
         let nombre_usuario = document.querySelector("#nombre_usuario").getAttribute('data');
@@ -37,28 +32,16 @@ let app = new Vue({
         let puntuacion = document.querySelector("#puntuacion").value;
 
        let data = {
-<<<<<<< HEAD
             "id_film": id_film,
             "nombre_usuario": nombre_usuario,
             "comentario": comentario,
             "puntuacion": puntuacion
-=======
-            "id_film":  id_film,
-            "nombre_usuario": nombre_usuario,
-            "comentario":  comentario,
-            "puntuacion":  puntuacion
->>>>>>> 0ba0e6b28217cf892510d31d9af68f1082bab966
         }
         console.log(data);
-        let Ulr = encodeURI("api/comentario")
 
-<<<<<<< HEAD
         let url = encodeURI("api/comentario");
 
         fetch(url, {
-=======
-        fetch(Url, {
->>>>>>> 0ba0e6b28217cf892510d31d9af68f1082bab966
             "method": "POST",
             "headers": {"Content-Type": "application/json"},       
             "body": JSON.stringify(data)
